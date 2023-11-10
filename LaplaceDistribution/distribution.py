c510b631d6327a92bbfc0361f393c825ea170663
tikhon <tikhon.radk@gmail.com>
2023-11-10 21:28:00 +0300

    laplace dist compleated

--- LaplaceDistribution/distribution.py
+++ LaplaceDistribution/distribution.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-class LaplaceDistribution:    
+
+class LaplaceDistribution:
     @staticmethod
     def mean_abs_deviation_from_median(x: np.ndarray):
         '''
@@ -10,7 +11,7 @@ class LaplaceDistribution:
         '''
         ####
         # Do not change the class outside of this block
-        # Your code here
+        return np.mean(np.abs(x - np.median(x, axis=0)), axis=0)
         ####
 
     def __init__(self, features):
@@ -20,10 +21,10 @@ class LaplaceDistribution:
         '''
         ####
         # Do not change the class outside of this block
-        self.loc = # YOUR CODE HERE
-        self.scale = # YOUR CODE HERE
-        ####
+        self.loc = np.median(features, axis=0)
+        self.scale = self.mean_abs_deviation_from_median(features)
 
+        ####
 
     def logpdf(self, values):
         '''
@@ -33,14 +34,14 @@ class LaplaceDistribution:
         '''
         ####
         # Do not change the class outside of this block
-        return 
+        return -np.log(2 * self.scale) - np.abs(values - self.loc) / self.scale
+
         ####
-        
-    
+
     def pdf(self, values):
         '''
         Returns probability density at every input value.
         Args:
             values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
         '''
-        return np.exp(self.logpdf(value))
+        return np.exp(self.logpdf(values))
