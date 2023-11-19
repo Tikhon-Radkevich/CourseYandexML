2caa15bb777d72ffe58ee29f6112bfedae85b455
tikhon <tikhon.radk@gmail.com>
2023-11-19 21:09:37 +0300

    boosting compleated

--- assignment06_boosting/boosting.py
+++ assignment06_boosting/boosting.py
@@ -3,31 +3,31 @@ import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
 
+
 class SimplifiedBoostingRegressor:
     def __init__(self):
         pass
-        
+
     @staticmethod
     def loss(targets, predictions):
-        loss = np.mean((targets - predictions)**2)
+        loss = np.mean((targets - predictions) ** 2)
         return loss
-    
+
     @staticmethod
     def loss_gradients(targets, predictions):
-        gradients = # YOUR CODE HERE
+        gradients = 2 * (targets - predictions)
         assert gradients.shape == targets.shape
         return gradients
-        
-        
+
     def fit(self, model_constructor, data, targets, num_steps=10, lr=0.1, max_depth=5, verbose=False):
         '''
         Fit sequence of models on the provided data.
         Model constructor with no parameters (and with no ()) is passed to this function.
-        If 
-        
+        If
+
         example:
-        
-        boosting_regressor = SimplifiedBoostingRegressor()    
+
+        boosting_regressor = SimplifiedBoostingRegressor()
         boosting_regressor.fit(DecisionTreeRegressor, X, y, 100, 0.5, 10)
         '''
         new_targets = targets
@@ -44,14 +44,14 @@ class SimplifiedBoostingRegressor:
             predictions = self.predict(data)
             self.loss_log.append(self.loss(targets, predictions))
             gradients = self.loss_gradients(targets, predictions)
-            new_targets = # YOUR CODE HERE
+            new_targets = gradients
         if verbose:
             print('Finished! Loss=', self.loss_log[-1])
         return self
-            
+
     def predict(self, data):
         predictions = np.zeros(len(data))
         for model in self.models_list:
-            predictions += # YOUR CODE HERE
+            predictions += model.predict(data) * self.lr
         return predictions
 
