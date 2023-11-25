9c3df41fe24d3ce017598b02b185f50ac108a83d
tikhon <tikhon.radk@gmail.com>
2023-11-25 11:59:46 +0300

    05 bagging compleated

--- assignment05_bagging_and_oob/bagging.py
+++ assignment05_bagging_and_oob/bagging.py
@@ -1,26 +1,26 @@
 import numpy as np
 
+
 class SimplifiedBaggingRegressor:
     def __init__(self, num_bags, oob=False):
         self.num_bags = num_bags
         self.oob = oob
-        
+
     def _generate_splits(self, data: np.ndarray):
         '''
         Generate indices for every bag and store in self.indices_list list
         '''
         self.indices_list = []
-        data_length = len(data)
         for bag in range(self.num_bags):
-            # Your Code Here
-        
+            self.indices_list.append(np.random.choice(len(data), len(data)))
+
     def fit(self, model_constructor, data, target):
         '''
         Fit model on every bag.
         Model constructor with no parameters (and with no ()) is passed to this function.
-        
+
         example:
-        
+
         bagging_regressor = SimplifiedBaggingRegressor(num_bags=10, oob=True)
         bagging_regressor.fit(LinearRegression, X, y)
         '''
@@ -28,44 +28,59 @@ class SimplifiedBaggingRegressor:
         self.target = None
         self._generate_splits(data)
         assert len(set(list(map(len, self.indices_list)))) == 1, 'All bags should be of the same length!'
-        assert list(map(len, self.indices_list))[0] == len(data), 'All bags should contain `len(data)` number of elements!'
+        assert list(map(len, self.indices_list))[0] == len(
+            data), 'All bags should contain `len(data)` number of elements!'
         self.models_list = []
         for bag in range(self.num_bags):
             model = model_constructor()
-            data_bag, target_bag = # Your Code Here
-            self.models_list.append(model.fit(data_bag, target_bag)) # store fitted models here
+            data_bag, target_bag = data[self.indices_list[bag]], target[self.indices_list[bag]]
+            self.models_list.append(model.fit(data_bag, target_bag))  # store fitted models here
         if self.oob:
             self.data = data
             self.target = target
-        
+
     def predict(self, data):
         '''
         Get average prediction for every object from passed dataset
         '''
-        # Your code here
-    
+        predictions = np.zeros(len(data))
+        for model in self.models_list:
+            predictions += model.predict(data)
+        return predictions / self.num_bags
+
     def _get_oob_predictions_from_every_model(self):
         '''
         Generates list of lists, where list i contains predictions for self.data[i] object
         from all models, which have not seen this object during training phase
         '''
         list_of_predictions_lists = [[] for _ in range(len(self.data))]
-        # Your Code Here
-        
+        for bag in range(self.num_bags):
+            for i in range(len(self.data)):
+                if i not in self.indices_list[bag]:
+                    list_of_predictions_lists[i].append(self.models_list[bag].predict(self.data[i].reshape(1, -1)))
         self.list_of_predictions_lists = np.array(list_of_predictions_lists, dtype=object)
-    
+
     def _get_averaged_oob_predictions(self):
         '''
         Compute average prediction for every object from training set.
         If object has been used in all bags on training phase, return None instead of prediction
         '''
         self._get_oob_predictions_from_every_model()
-        self.oob_predictions = # Your Code Here
-        
-        
+        self.oob_predictions = []
+        for pred_list in self.list_of_predictions_lists:
+            if len(pred_list) == 0:
+                self.oob_predictions.append(None)
+            else:
+                self.oob_predictions.append(np.mean(pred_list))
+
+        self.oob_predictions = np.array(self.oob_predictions)
+
     def OOB_score(self):
         '''
         Compute mean square error for all objects, which have at least one prediction
         '''
         self._get_averaged_oob_predictions()
-        return # Your Code Here
+        con = self.oob_predictions != None
+        oob_targets = self.target[con]
+        oob_predictions = self.oob_predictions[con]
+        return np.mean(np.square(oob_predictions - oob_targets))
